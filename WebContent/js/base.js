5b9d6088c8e3c891ecaa68e6f3538341bbecdb35
leedow <644743991@qq.com>
2014-09-14 21:01:45 +0800

    11
    111

--- WebContent/js/base.js
+++ WebContent/js/base.js
@@ -2,9 +2,6 @@
 var map, toolbar, symbol, geomTask;
 require([
     /*搜索部分*/
-<<<<<<< HEAD
-    "esri/map", 
-=======
 	"esri/dijit/Geocoder",
 	"esri/map",
 	"esri/graphic",
@@ -15,42 +12,14 @@ require([
 	"dojo/query",
 	"dojo/_base/Color",
 	/*编辑部分*/
->>>>>>> origin/master
     "esri/toolbars/draw",
-    "esri/graphic",
-    "esri/symbols/SimpleMarkerSymbol",
     "esri/symbols/SimpleLineSymbol",
     "esri/symbols/SimpleFillSymbol",
     "dojo/parser", 
     "dijit/registry",
-    
-    
     "dijit/layout/BorderContainer", 
     "dijit/layout/ContentPane", 
     "dijit/form/Button", 
-<<<<<<< HEAD
-    "dijit/WidgetSet", 
-    "dojo/domReady!",
-    
-	
-	"esri/dijit/Geocoder",
-	"esri/geometry/screenUtils",
-	"dojo/dom",
-	"dojo/dom-construct",
-	"dojo/query",
-	"dojo/_base/Color"
-    
-    ], function(
-		Map, Draw, Graphic,
-        SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol,
-        parser, registry, BorderContainer, ContentPane, Button, WidgetSet, domReady, 
-        
-	    Geocoder,
-	    screenUtils,
-	    dom, domConstruct, query, Color
-        
-         
-=======
     "dijit/WidgetSet",
     "dojo/domReady!"
 	], function(
@@ -58,7 +27,6 @@ require([
 	    Graphic, SimpleMarkerSymbol, screenUtils,
 	    dom, domConstruct, query, Color, /*draw*/Draw, SimpleLineSymbol, SimpleFillSymbol,
         parser, registry
->>>>>>> origin/master
 	) { 
     // create a map and instance of the geocoder widget here
 	parser.parse();
@@ -67,14 +35,11 @@ require([
     	center: [ -100, 40 ], 
     	zoom: 10
     });
-	
-	edit.init(Map, Draw, Graphic,
-	        SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol,
-	        parser, registry);
-	
     search.init(Map, Geocoder,
-    	    Graphic, SimpleMarkerSymbol, screenUtils,
-    	    dom, domConstruct, query, Color
+	    Graphic, SimpleMarkerSymbol, screenUtils,
+	    dom, domConstruct, query, Color
     ); 
-    
+    edit.init(Map, Draw, Graphic,
+            SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol,
+            parser, registry);
 });
