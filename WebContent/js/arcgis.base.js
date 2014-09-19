9c11b594201bf7710cd2c2bea72f7bbf0dd963a3
summer jaydon <1031352017@qq.com>
2014-09-19 10:37:10 +0800

    Revert "增"
    This reverts commit a96582d55e7ab23bffc520177a6dbbbf82d7074c.

--- WebContent/js/arcgis.base.js
+++ WebContent/js/arcgis.base.js
@@ -47,10 +47,8 @@ map.init = function(){
  		parser.parse().then(function(){
  			layout.init();//初始化图层
  			edit.init();
- 			//attachment.init();
+ 			attachment.init();
  			search.init();
- 			features.init();
- 			track.init();
  		});	
  		
   	});
