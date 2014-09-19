76a042c9eaad299a5d46ab2c7dee616011157e31
summer jaydon <1031352017@qq.com>
2014-09-19 14:25:17 +0800

    modify measure

--- WebContent/js/arcgis.measure.js
+++ WebContent/js/arcgis.measure.js
@@ -37,33 +37,37 @@ measure.init = function(){
         //This sample may require a proxy page to handle communications with the ArcGIS Server services. You will need to  
         //replace the url below with the location of a proxy on your machine. See the 'Using the proxy page' help topic 
         //for details on setting up a proxy page.
-        //esriConfig.defaults.io.proxyUrl = "/proxy";
-        //esriConfig.defaults.io.alwaysUseProxy = false;
+        esriConfig.defaults.io.proxyUrl = "/proxy";
+        esriConfig.defaults.io.alwaysUseProxy = false;
 
         //This service is for development and testing purposes only. We recommend that you create your own geometry service for use within your applications
-        //esriConfig.defaults.geometryService = new GeometryService("http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer");
+        esriConfig.defaults.geometryService = new GeometryService("http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer");
 
+        var sfs = new SimpleFillSymbol(
+            "solid",
+            new SimpleLineSymbol("solid", new Color([195, 176, 23]), 2), 
+            null
+          );
 
-        
+          var parcelsLayer = new FeatureLayer("http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Louisville/LOJIC_LandRecords_Louisville/MapServer/0", {
+            mode: FeatureLayer.MODE_ONDEMAND,
+            outFields: ["*"]
+          });
+          parcelsLayer.setRenderer(new SimpleRenderer(sfs));
+          //global_map.addLayers([parcelsLayer]);
 
-        var parcelsLayer = new FeatureLayer("http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Louisville/LOJIC_LandRecords_Louisville/MapServer/0", {
-          mode: FeatureLayer.MODE_ONDEMAND,
-          outFields: ["*"]
-        });
+          //dojo.keys.copyKey maps to CTRL on windows and Cmd on Mac., but has wrong code for Chrome on Mac
+          var snapManager = global_map.enableSnapping({
+            snapKey: has("mac") ? keys.META : keys.CTRL
+          });
+          var layerInfos = [{
+            layer: parcelsLayer
+          }];
+          snapManager.setLayerInfos(layerInfos);
 
-        //dojo.keys.copyKey maps to CTRL on windows and Cmd on Mac., but has wrong code for Chrome on Mac
-        var snapManager = global_map.enableSnapping({
-          snapKey: has("mac") ? keys.META : keys.CTRL
-        });
-        var layerInfos = [{
-          layer: parcelsLayer
-        }];
-        //snapManager.setLayerInfos(layerInfos);
-
-        var measurement = new Measurement({
-        	global_map: global_map
-        }, dom.byId("measurementDiv"));
-
-        measurement.startup();
+          var measurement = new Measurement({
+            map: global_map
+          }, dom.byId("measurementDiv"));
+          measurement.startup();
       });
 }
